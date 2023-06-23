91fe351d2ab8a1da0e4aa1e301284ecf48ed80eb
Andres620 <andres.1701625573@ucaldas.edu.co>
2023-06-23 13:20:36 -0500

    Se generaron los modelos de entidad a parir del esquema de la base de datos

---
+++ backend/ProductStore/ProductStore.Repository.Implementation/DataModel/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductStore.Repository.Implementation.DataModel;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public string Precio { get; set; } = null!;
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+}
---
+++ backend/ProductStore/ProductStore.Repository.Implementation/DataModel/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductStore.Repository.Implementation.DataModel;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string CorreoElectronico { get; set; } = null!;
+
+    public string Contrasena { get; set; } = null!;
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+}
