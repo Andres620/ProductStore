3d01e343d179357bebdf0fd09969179f12a4644f
Andres620 <andres.1701625573@ucaldas.edu.co>
2023-06-23 11:00:02 -0500

    Se agregaron los modelos a las distintas capas de la aplicacion

---
+++ backend/ProductStore/ProductStore.Application.Contracts/DTO/Core/UsuarioDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Application.Contracts.DTO.Core
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
---
+++ backend/ProductStore/ProductStore/Models/Core/PedidoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductStore.Models.Core
+{
+    public class PedidoModel
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
